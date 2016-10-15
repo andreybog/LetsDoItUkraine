37e2d9fe07ecf752813e3fa3b87bc938a8b9a144
Gutkin Anton <anton.gutkin@gmail.com>
2016-10-15 21:56:45 +0300

    add user screen

---
+++ LetsDoItUkraine/CleaningCell.swift
@@ -0,0 +1,24 @@
+//
+//  CleaningCell.swift
+//  LetsDoItUkraine
+//
+//  Created by Anton A on 11.10.16.
+//  Copyright © 2016 goit. All rights reserved.
+//
+
+import UIKit
+
+class CleaningCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ LetsDoItUkraine/NoCleaningCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoCleaningCell.swift
+//  LetsDoItUkraine
+//
+//  Created by Anton A on 10.10.16.
+//  Copyright © 2016 goit. All rights reserved.
+//
+
+import UIKit
+
+class NoCleaningCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
