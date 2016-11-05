c46e946c3d833b9747457781ead2fbe5fcc85f6d
Anton A <anton.gutkin@gmail.com>
2016-11-05 15:36:34 +0200

    CreatCleaningViewController add search location

--- LetsDoItUkraine/CreateCleaningViewController.swift
+++ LetsDoItUkraine/CreateCleaningViewController.swift
@@ -7,29 +7,96 @@
 //
 
 import UIKit
+import GooglePlaces
 
-class CreateCleaningViewController: UIViewController {
-
+class CreateCleaningViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate, SearchResultsDelegate, UISearchBarDelegate {
+    let kSearchResultCellIdentifier = "searchResultCell"
+    
+    @IBOutlet weak var dateAndTimeTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    @IBOutlet var addPhotoButtons: [UIButton]!
+    @IBOutlet weak var addressSearchBar: UISearchBar!
+    
+    var resultArray = [String]()
+    let searchController = SearchResultsController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        searchController.delegate = self
+        addressSearchBar.delegate = self
+        addressSearchBar.layer.borderWidth = 0.01
+        addressSearchBar.layer.borderColor = UIColor.white.cgColor
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+            }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+        }
+    // MARK: - UITableViewDelegate
+    
+    // MARK: - UISearchBarDelegate
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        
+        if searchBar == self.addressSearchBar {
+            let controller = UISearchController(searchResultsController: self.searchController)
+            controller.hidesNavigationBarDuringPresentation = true
+            controller.searchBar.delegate = self
+            controller.searchBar.text = self.addressSearchBar.placeholder
+            present(controller, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let placesClient = GMSPlacesClient()
+        placesClient.autocompleteQuery(searchText, bounds: nil, filter: nil) { [unowned self] (results, error:Error?) in
+            self.resultArray.removeAll()
+            if results == nil {
+                return
+            }
+            for result in results!{
+                self.resultArray.append(result.attributedFullText.string)
+            }
+            self.searchController.reloadDataWith(Array: self.resultArray)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Actions
+    
+
+    @IBAction func addPhotoButtonDidTapped(_ sender: Any) {
+    }
+    
+    
+    // MARK: - LoadData
+    // MARK: - SearchResultDelegate
+    func pass(longtitude lon: Double, andLatitude lat: Double, andTitle title: String) {
+        addressSearchBar.placeholder = title
+    }
+
+    
+    // MARK: - AutoComplete
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
     }
-    */
-
 }
